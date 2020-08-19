b800052211161d18a2bf005c3449339e06f7d075
Nandinisomashekar <nandiniknklr@gmail.com>
2020-08-19 22:09:11 +0530

    Update 1.Check Brackets in code.py

--- 1.Check Brackets in code.py
+++ 1.Check Brackets in code.py
@@ -3,8 +3,11 @@ def check_parentheses(S):
     brackets = {"(": ")", "[": "]",  "{": "}"}
     open_brackets = ['(','[','{']
     stack = []
+    special_char = [":",";","/","#","@","!","$","%","^","&","*"]
     for bracket in S:
-        if bracket in open_brackets:
+        if 122>=ord(bracket)>=97 or 90>=ord(bracket)>=65 or 57>=ord(bracket)>=48 or bracket in special_char:
+            continue
+        elif bracket in open_brackets:
             stack.append(bracket)
         elif stack and bracket == brackets[stack[-1]]:
                 stack.pop()
