0d6cdb0569b82b76d21ab62f52ba4e3999beb94e
Nandinisomashekar <nandiniknklr@gmail.com>
2020-08-20 20:46:31 +0530

    Update 1.Check Brackets in code.py

--- 1.Check Brackets in code.py
+++ 1.Check Brackets in code.py
@@ -1,20 +1,17 @@
 S = input()
-def check_parentheses(S):
-    brackets = {"(": ")", "[": "]",  "{": "}"}
-    open_brackets = ['(','[','{']
-    stack = []
-    special_char = [":",";","/","#","@","!","$","%","^","&","*"]
-    for bracket in S:
-        if 122>=ord(bracket)>=97 or 90>=ord(bracket)>=65 or 57>=ord(bracket)>=48 or bracket in special_char:
-            continue
-        elif bracket in open_brackets:
-            stack.append(bracket)
-        elif stack and bracket == brackets[stack[-1]]:
-                stack.pop()
-        else:
-            return False
-    return stack == []
-if check_parentheses(S):
+brackets = {"(": ")", "[": "]",  "{": "}"}
+open_brackets = ['(','[','{']
+stack = []
+special_char = [":",";","/","#","@","!","$","%","^","&","*"]
+for index,bracket in enumerate(S):
+    if 122>=ord(bracket)>=97 or 90>=ord(bracket)>=65 or 57>=ord(bracket)>=48 or bracket in special_char:
+        continue
+    elif bracket in open_brackets:
+        stack.append(bracket)
+    elif stack and bracket == brackets[stack[-1]]:
+            stack.pop()
+    else:
+        print(index+1)
+        
+if len(stack)==0:
     print("Success")
-else:
-    print("1")
